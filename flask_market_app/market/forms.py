54f1f0a84175c2f21fbdf499ae9a3b0a12c48ab9
Raul <ratiuraul@github.com>
2025-03-10 17:06:08 +0200

    add email and username validations

--- flask_market_app/market/forms.py
+++ flask_market_app/market/forms.py
@@ -1,8 +1,21 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, validators
+from market.models import User
 
 
 class RegisterForm(FlaskForm):
+
+    def validate_username(self, username_to_check):
+        user = User.query.filter_by(username=username_to_check.data).first()
+        if user:
+            raise validators.ValidationError("Username already exists !")
+
+    def validate_email_address(self, email_address_to_check):
+        email_address = User.query.filter_by(
+            email_address=email_address_to_check.data).first()
+        if email_address:
+            raise validators.ValidationError("Email address already exists !")
+
     username = StringField(label='User Name:',
                            validators=[
                                validators.Length(min=2, max=30),
--- flask_market_app/market/routes.py
+++ flask_market_app/market/routes.py
@@ -1,5 +1,5 @@
 from market import app, db
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, flash
 from market.models import Item, User
 from market.forms import RegisterForm
 
@@ -30,5 +30,5 @@ def register_page():
         return redirect(url_for('market_page'))
     if form.errors:
         for err_msg in form.errors.values():
-            print(f"Error creating user : {err_msg}")
+            flash(f"Error creating user : {err_msg}", category='danger')
     return render_template('register.html', form=form)
