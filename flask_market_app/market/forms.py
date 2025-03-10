33119c3f996293ea65a1adcf75101740087aedfb
Raul <ratiuraul@github.com>
2025-03-10 16:26:58 +0200

    Validate users registration

--- flask_market_app/market/forms.py
+++ flask_market_app/market/forms.py
@@ -1,10 +1,26 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, validators
 
 
 class RegisterForm(FlaskForm):
-    username = StringField(label='User Name:')
-    email_address = StringField(label='Email Address:')
-    password1 = PasswordField(label='Password:')
-    password2 = PasswordField(label='Confirm Password:')
+    username = StringField(label='User Name:',
+                           validators=[
+                               validators.Length(min=2, max=30),
+                               validators.DataRequired()
+                           ])
+    email_address = StringField(label='Email Address:',
+                                validators=[
+                                    validators.Email(),
+                                    validators.DataRequired()
+                                ])
+    password1 = PasswordField(label='Password:',
+                              validators=[
+                                  validators.Length(min=6),
+                                  validators.DataRequired()
+                              ])
+    password2 = PasswordField(label='Confirm Password:',
+                              validators=[
+                                  validators.EqualTo('password1'),
+                                  validators.DataRequired()
+                              ])
     submit = SubmitField(label='Create Account')
